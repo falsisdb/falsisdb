25c7f95f69e84343918cf0666ce2a5dd0af3c20d
Falsis <63756985+falsisdev@users.noreply.github.com>
2021-06-16 20:26:28 +0300

    Update test.js

--- test.js
+++ test.js
@@ -1,4 +1,21 @@
 const falsis = require("./src/index.js")
 const db = new falsis('./database.json')
-console.log(db.sqrt(81))
-//Bunu yazan tosun (falsis eheheu) okuyanın amna kosun
+db.on({
+	type: "ready",
+	status:"aktif",
+	code:`console.log("Ready eventi çalışıyor")`
+})
+db.on({
+	type:"dataSet",
+	status:"aktif",
+	code:`console.log("Veri tabanına yeni veri eklendi")`
+})
+db.on({
+	type:"dataDelete",
+	status:"aktif",
+	code:`console.log("Veri tabanından veri silindi")`
+})
+db.set("veri_adı", "veri_değeri")
+db.delete("veri_adı")
+console.log(db.info)
+//Bunu yazan tosun (falsis eheheu) okuyanın aaa--
