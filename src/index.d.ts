804f7fb09e16060630d54c0c2d8ea352e9857973
Falsis <63756985+falsisdev@users.noreply.github.com>
2021-06-19 17:23:56 +0300

    edit: typescript types

--- src/index.d.ts
+++ src/index.d.ts
@@ -1,17 +1,25 @@
-declare class Database {
+declare class falsisdb {
     constructor(filePath: string);
     fetchDataFromFile(): void;
-    saveDataToFile(): void;
+    kaydet(): void;
+    on(event: string, type: string, status: string, code: string): void;
     get(key: string): any;
     fetch(key: string): any;
     has(key: string): boolean;
     set(key: string, value: any): void;
     delete(key: string): void;
+    conc(key: string, count: string); any;
+    multi(key: string, count: number): void;
+    divide(key: string, count: number): void;
     sum(key: string, count: number): void;
     sub(key: string, count: number): void;
     push(key: string, element: any): void;
+    sqrt(sayi: number): any;
+    math(key: number, islem: any, key2: number): any;
+    includes(key: any);
+    all(): void;
     clear(): void;
 }
 
-export = Database;
+export = falsisdb;
 
