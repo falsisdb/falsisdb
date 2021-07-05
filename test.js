5659362fc434dfd2346dd05ec4e9110326828271
Falsis <63756985+falsisdev@users.noreply.github.com>
2021-07-05 11:24:11 +0300

    Update test.js

--- test.js
+++ test.js
@@ -8,15 +8,12 @@ db.on({
 db.on({
 	type:"dataSet",
 	status:"aktif",
-	code:`console.log("Veri tabanına yeni veri eklendi")`
+	code:`console.log("Veri tabanına %value% değeri olan %key% verisi eklendi")`
 })
 db.on({
 	type:"dataDelete",
 	status:"aktif",
-	code:`console.log("Veri tabanından veri silindi")`
+	code:`console.log("Veri tabanından %value% değeri olan %key% verisi silindi.")`
 })
 db.set("veri_adı", "veri_değeri")
 db.delete("veri_adı")
-console.log(db.info)
-console.log(db.all())
-//Bunu yazan tosun (falsis eheheu) okuyanın aaa--
