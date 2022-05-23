40944319c0d2cad4256a8b070834f54ad09d5fe0
Falsis <keremtokul@hotmail.com>
2022-05-23 14:18:57 +0300

    chore: minor changes

--- test.js
+++ test.js
@@ -1,22 +1,13 @@
 const falsis = require("./src/index.js")
 const db = new falsis()
-db.set("veri_adı", "veri_değeri") //undefined
-db.get("veri_adı") //veri_değeri
-if(db.has("veri_adı") === true) {
-console.log("Değişken veri tabanı dosyasında bulunuyor.")
-}else if(db.has("veri_adı") === false) {
-console.log("Değişken veri tabanı dosyasında bulunmuyor.")
-}
-db.delete("veri_adı") //undefined
-if(db.has("veri_adı") === true) {
-console.log("Değişken veri tabanı dosyasında bulunuyor.")
-}else if(db.has("veri_adı") === false) {
-console.log("Değişken veri tabanı dosyasında bulunmuyor.")
-}
-//buraya kadar çalışıyor aşağısı hatalı
-//EVENTLER HATALI (EVENT EMITTER ILE ILGILI)
 db.on({
-	type: "ready",
-	status:"aktif",
-	code:`console.log("Ready eventi çalışıyor")`
+	"status": "aktif",
+	"type": "ready",
+	"code": `console.log("FalsisDB: Database başlatıldı")`
 })
+db.on({
+	"type": "dataSet",
+	"status": "aktif",
+	"code": `console.log("FalsisDB: Database'e değeri %value% olan %key% isimli veri kaydedildi.")`
+})
+db.set("Variable name", "Variable value")
