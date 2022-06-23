13404f0829a66e865e8cff969e673caaafd8fcd6
berat141 <79844920+berat141@users.noreply.github.com>
2022-06-23 15:41:06 +0300

    Update index.js
    Event sistemi düzeltildi
    has, includesKey, includesValue, hasValue artık sadece key veya value değil ikisinide döndürebilecek
    includes kaldırıldı
    Event sisteminin ne kadar sürede bir kontrol yapacağı artık ayarlanabilir (süre ms cinsinden)
    Backup artık key ve valuenin ikisini birden obje olarak da kayıt edecek

--- src/index.js
+++ src/index.js
@@ -1,7 +1,9 @@
 const fs = require("fs");
 let clearfunc;
 let check = null
+let willBeExecutedDatas = []
 let deleteEventCheck = null
+let willBeExecutedDeleteDatas = []
 let data;
 let type;
 let backup;
@@ -10,6 +12,7 @@ let btime;
 let backupkeys = []
 let backupvalues = []
 let backupcount = 0
+let backupdata = {}
 
 function padTo2Digits(num) {
   return num.toString().padStart(2, '0');
@@ -147,13 +150,19 @@ class database extends EventEmitter{
       }
        setInterval(() => {
           if(check != null){
-        this.emit('dataSet', check)
+            for(data of willBeExecutedDatas) {
+        this.emit('dataSet', data)
+            }
         check = null
+        willBeExecutedDatas = []
           } else if(deleteEventCheck != null) {
-            this.emit('dataDelete', deleteEventCheck)
+            for(data of willBeExecutedDeleteDatas) {
+        this.emit('dataDelete', data)
+            }
             deleteEventCheck = null
+            willBeExecutedDeleteDatas = []
           }
-        }, 5000)
+        }, construct.eventInterval || 100)
     }
   }
     fetchDataFromFile() {
@@ -184,18 +193,20 @@ class database extends EventEmitter{
         if(!key) throw Error("❌ FalsisDB Hatası: Veri Tabanından Çekilecek Veri Bulunamadı. Lütfen Çekmek İstediğiniz Veriyi Girin.")
         return this.data[key];
     }
-    has(key, returnValue=false) {
+    has(key, returnDatas=false) {
         if(!key) throw Error("❌ FalsisDB Hatası: Veri Tabanında Varlığı Kontrol Edilecek Veri Bulunamadı. Lütfen Şartlanacak Veriyi Girin.")
 
-        if(returnValue === false){
+        if(returnDatas === false){
         return Boolean(this.data[key]);
         } else {
           let result = Boolean(this.data[key]);
-          let values = Object.entries(JSON.parse(fs.readFileSync(this.jsonFilePath, "utf-8"))).filter(x=>x[0] === key).map(x=>x[1])
-
+          let data = Object.entries(JSON.parse(fs.readFileSync(this.jsonFilePath, "utf-8"))).filter(x=>x[0] === key).map(x=>x)
+          let obj = {}
+          obj[data[0][0]] = data[0][1]
+          
           return{
             result:result,
-            values:values
+            data:obj
           }
         }
     }
@@ -218,8 +229,10 @@ class database extends EventEmitter{
           oldValue: old,
           value: value
         }
+          willBeExecutedDatas.push(check)
         if(backup == false){}else{
           backupcount += 1
+          backupdata[key] = value
           backupkeys.push(key)
           backupvalues.push(value)
           if(backupcount == btime){
@@ -228,13 +241,14 @@ class database extends EventEmitter{
               this.bdata[`Back-Up-${Math.floor(Math.random() * 1000000000000)}`] = {
                 date: formatDate(new Date()),
                 keys: backupkeys,
-                values: backupvalues
+                values: backupvalues,
+                data:backupdata
               }
               this.yedekle();
             }else if(btype == "txt") {
               fs.writeFileSync(backup, `Back-Up-${Math.floor(Math.random() * 1000000000000)} | ${formatDate(new Date())} | ${backupkeys} | ${backupvalues}`)
             }
-            console.log("📝 Falsisdb Bilgilendirme: Yedekleme Alındı. Yedek ismi: Back-Up-" + Math.floor(Math.random() * 1000000000000) + ".")
+            console.log("Backup Alındı")
           }
         }
         }
@@ -253,6 +267,7 @@ class database extends EventEmitter{
           key: key,
           value:val
         }
+          willBeExecutedDeleteDatas.push(deleteEventCheck)
         }
     }
 
@@ -403,53 +418,70 @@ class database extends EventEmitter{
         }
         }
     }
-     includes(key) {
-        if(!key) {
-            throw new TypeError("❌ FalsisDB Hatası: Veri Tabanında Varlığı Kontrol Edilecek Veri Bulunamadı. Lütfen Şartlanacak Veriyi Girin.") 
-        }
-        return fs.readFileSync(this.jsonFilePath).includes(key)
-    }
         all() {
         if(!this.jsonFilePath) {
             throw new TypeError("❌ FalsisDB Hatası: Veri Tabanı Dosyası Bulunamadı. Lütfen Geliştiriciler İle İletişime Geçin.")
         }
          return fs.readFileSync(`${this.jsonFilePath}`, "utf8")
         }
-        includesKey(key) {
+        includesKey(key,returnDatas=false) {
           if(!key) {
             throw new Error("❌ FalsisDB Hatası: Veri Tabanında Varlığı Kontrol Edilecek Veri Bulunamadı. Lütfen Şartlanacak Veriyi Girin.")
           } else {
-          return Object.entries(JSON.parse(fs.readFileSync(this.jsonFilePath, "utf-8")))
-          .filter(x=>x[0].includes(key)).length === 0 ? false : true
+            let data = Object.entries(JSON.parse(fs.readFileSync(this.jsonFilePath, "utf-8"))).filter(x=>x[0].includes(key))
+            if(returnDatas = false){
+return data.length > 0 ? true : false
+            }else {
+              let obj = {};
+              data.forEach(x=>{
+                obj[x[0]] = x[1]
+              })
+              return {
+                result:data.length > 0 ? true : false,
+                data:obj
+              }
+            
+            }
           }
         }
-        includesValue(value) {
+        includesValue(value, returnDatas=false) {
           if(!value) {
             throw new Error("❌ FalsisDB Hatası: Veri Tabanında Varlığı Kontrol Edilecek Veri Değeri Bulunamadı. Lütfen Şartlanacak Verinin Değerini Girin.")
           } else {
-          return Object.entries(JSON.parse(fs.readFileSync(this.jsonFilePath, "utf-8")))
-          .filter(x=>x[1].includes(value)).length === 0 ? false : true
+          let data = Object.entries(JSON.parse(fs.readFileSync(this.jsonFilePath, "utf-8"))).filter(x=>x[1].includes(value))
+            if(returnDatas = false){
+return data.length > 0 ? true : false
+            }else {
+              let obj = {};
+              data.forEach(x=>{
+                obj[x[0]] = x[1]
+              })
+              return {
+                result:data.length > 0 ? true : false,
+                data:obj
+              }
+            
+            }
           }
         }
 
-        hasValue(value, returnKey=false){
+        hasValue(value, returnDatas=false){
           if(!value){
             throw new Error("❌ FalsisDB Hatası: Veri Tabanında Varlığı Kontrol Edilecek Veri Değeri Bulunamadı. Lütfen Şartlanacak Verinin Değerini Girin.")
           }
 
-          if(returnKey == false){
-          return Object.entries(JSON.parse(fs.readFileSync(this.jsonFilePath, "utf-8")))
-          .filter(x=>x[1] === value).length === 0 ? false : true
-          } else {
-            let result = Object.entries(JSON.parse(fs.readFileSync(this.jsonFilePath, "utf-8")))
-          .filter(x=>x[1] === value).length === 0 ? false : true
-
-           let keys = Object.entries(JSON.parse(fs.readFileSync(this.jsonFilePath, "utf-8")))
-          .filter(x=>x[1] === value).map(x=>x[0])
-
+          let data = Object.entries(JSON.parse(fs.readFileSync(this.jsonFilePath, "utf-8"))).filter(x=>x[1] === value)
+          
+          if(returnDatas == false){
+          return data.length === 0 ? false : true
+          } else {          
+          let obj = {}
+            data.forEach(x=> {
+              obj[x[0]] = x[1]
+            })
             return{
-              result:result,
-              keys: keys
+              result:data.length > 0 ? true : false,
+              data:obj
             }
           }
         }
