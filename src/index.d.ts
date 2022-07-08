9b86cfeb393681df50b2ab5375beeb9249e7a696
Falsis <63756985+falsisdev@users.noreply.github.com>
2022-07-08 15:21:38 +0300

    chore: edited d.ts file for TypeScript types

--- src/index.d.ts
+++ src/index.d.ts
@@ -1,11 +1,9 @@
 declare class falsisdb {
-    constructor(filePath: string);
+    constructor(filePath: string, fileType: string, backupTime: number, backupPath: string); void;
     fetchDataFromFile(): void;
-    kaydet(): void;
-    on(event: string, type: string, status: string, code: string): void;
+    kaydet(key: any, value: any, type: any): void;
     get(key: string): any;
-    fetch(key: string): any;
-    has(key: string): boolean;
+    has(key: string, returnDatas: boolean): boolean;
     set(key: string, value: any): void;
     delete(key: string): void;
     conc(key: string, count: string); any;
@@ -14,12 +12,16 @@ declare class falsisdb {
     sum(key: string, count: number): void;
     sub(key: string, count: number): void;
     push(key: string, element: any): void;
-    sqrt(sayi: number): any;
-    math(key: number, islem: any, key2: number): any;
-    includes(key: any);
+    includesValue(value: string, returnDatas: boolean): boolean;
+    hasValue(value: string, returnDatas: boolean): boolean;
+    keys(): void;
+    values(): void;
+    find(fn: any, thisArg: any): void;
+    filter(fn: any, thisArg: any): void;
+    filterKey(fn: any, thisArg: any): void;
+    info(): void;
     all(): void;
     clear(): void;
 }
 
 export = falsisdb;
-
