7a8eec7db32e333892678abefabf4ab1f630bb00
Falsis <63756985+falsisdev@users.noreply.github.com>
2023-02-26 18:00:37 +0300

    chore: edited index.d.ts

--- src/index.d.ts
+++ src/index.d.ts
@@ -1,34 +1,6 @@
 declare module 'falsisdb' {
-    export class JSONDatabase  {
+    interface Database  {
         constructor(options?: {filePath?: string, backup?: any, time?: number});
-        fetchDataFromFile(): void;
-        kaydet(key: any, value: any, type: any): void;
-        get(key: string): any;
-        has(key: string, returnDatas: boolean): boolean;
-        set(key: string, value: any): void;
-        delete(key: string): void;
-        conc(key: string, count: string): void;
-        multi(key: string, count: number): void;
-        divide(key: string, count: number): void;
-        sum(key: string, count: number): void;
-        sub(key: string, count: number): void;
-        push(key: string, element: any): void;
-        includesValue(value: string, returnDatas: boolean): boolean;
-        hasValue(value: string, returnDatas: boolean): boolean;
-        keys(): void;
-        values(): void;
-        find(fn: any, thisArg: any): void;
-        filter(fn: any, thisArg: any): void;
-        filterKey(fn: any, thisArg: any): void;
-        info(): void;
-        all(): void;
-        clear(): void;
-    }
-
-    export class YAMLDatabase  {
-        constructor(options?: {filePath?: string, backup?: any, time?: number});
-        fetchDataFromFile(): void;
-        kaydet(key: any, value: any, type: any): void;
         get(key: string): any;
         has(key: string, returnDatas: boolean): boolean;
         set(key: string, value: any): void;
@@ -51,3 +23,5 @@ declare module 'falsisdb' {
         clear(): void;
     }
 }
+export class JSONDatabase implements Database { }
+export class YAMLDatabase implements Database { }
